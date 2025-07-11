c5d6b360bfc50bb4a0bdfd9b8bdceb93e8fea7e8
Paul Haardt <haardt.paul@gmail.com>
2025-07-11 10:14:44 +0200

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,36 @@
-untar the Unity.tar.gz file in the build directory. Create this directory if it does not exist.
-Also copy the root .env file to the build directory.
+#!/bin/bash
 
-mkdir -p build
-tar -xzf Unity.tar.gz -C build
-cp .env build/.env
+# Function to untar Unity.tar.gz into build directory and copy .env file
+untar_and_setup() {
+    mkdir -p build
+    tar -xzf Unity.tar.gz -C build
+    cp .env build/.env
+    echo "Extracted Unity.tar.gz to build directory"
+}
+
+# Function to create a tar.gz of the build directory
+create_tar() {
+    if [ -d "build" ]; then
+        tar -czf Unity.tar.gz -C build .
+        echo "Created Unity.tar.gz from build directory"
+    else
+        echo "Error: build directory does not exist"
+        exit 1
+    fi
+}
+
+# Check command line arguments
+case "$1" in
+    "untar")
+        untar_and_setup
+        ;;
+    "tar")
+        create_tar
+        ;;
+    *)
+        echo "Usage: $0 [untar|tar]"
+        echo "  untar: Extract Unity.tar.gz to build directory"
+        echo "  tar: Create Unity.tar.gz from build directory"
+        exit 1
+        ;;
+esac
